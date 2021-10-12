66a1990225599c659b6db5ae3a33389da2d66316
DragonOfDissent <dvpsherman@gmail.com>
2021-10-12 16:39:28 -0500

    adding comments

--- src/main.kt
+++ src/main.kt
@@ -1,9 +1,11 @@
 fun main() {
+    // Example employees
     val emp1 = Employee("Jane Doe", Employee.PositionTitle.ADMINISTRATION, true, 27.00, 1);
     val emp2 = Employee("Larry Jackson", Employee.PositionTitle.PRODUCTION, false, 22.00, 2);
     val emp3 = Employee("John Smith", Employee.PositionTitle.MAINTENANCE, false, 21.00, 3);
 
 
+    //Call for the employee info to be displayed and calculate their weekly pay below each
     emp1.display();
     emp1.calculate(45);
     emp2.display();
